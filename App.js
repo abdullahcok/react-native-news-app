5507d7f6994ec78d1d5d6b34828762f136983c28
Abdullah <abdullahcok@yandex.com>
2024-01-01 18:45:55 +0300

    news here

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  Image,
+  StyleSheet,
+  FlatList,
+  ScrollView,
+  Dimensions,
+} from 'react-native';
+import NewsCard from './components/NewsCard.js';
+
+import news_data from './news_data.json';
+import news_banner_data from './news_banner_data.json';
+
+function App() {
+  const renderNews = ({item}) => <NewsCard news={item} />;
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.headerText}>News</Text>
+      <FlatList
+        ListHeaderComponent={() => (
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            {news_banner_data.map(bannerNews => (
+              <Image
+                style={styles.banner_image}
+                source={{uri: bannerNews.imageUrl}}
+              />
+            ))}
+          </ScrollView>
+        )}
+        keyExtractor={item => item.u_id.toString()}
+        data={news_data}
+        renderItem={renderNews}
+      />
+    </SafeAreaView>
+  );
+}
+
+
+
+export default App;
